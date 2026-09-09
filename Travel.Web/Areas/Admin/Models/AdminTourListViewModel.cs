35001f7d750973ab14cc93f28ec1b27af5858a6d
berkantgll <berkantgll13@gmail.com>
2026-09-09 22:52:41 +0300

    .

---
+++ Travel.Web/Areas/Admin/Models/AdminTourListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Travel.Web.DTOs.TourDtos;
+
+namespace Travel.Web.Areas.Admin.Models
+{
+    public class AdminTourListViewModel
+    {
+        public ResultTourDto Tour { get; set; }
+        public int ReservationCount { get; set;}
+    }
+}
